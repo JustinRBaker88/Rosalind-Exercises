2f1c288ef790d5ceabe9cc36bf81e7660f126e73
Justin Baker <JustinRBaker88@gmail.com>
2018-01-22 17:40:54 -0800

    Pseudo code for algorithm. TO DO: Implement

---
+++ Problem 11/MotifEnumeration.py
@@ -0,0 +1,10 @@
+    def motif_enumeration(Dna, k, d)
+        Patterns <- an empty set
+        for each k-mer Pattern in Dna
+            for each k-mer Pattern’ differing from Pattern by at most d
+              mismatches
+                if Pattern' appears in each string from Dna with at most d
+                mismatches
+                    add Pattern' to Patterns
+        remove duplicates from Patterns
+        return Patterns
