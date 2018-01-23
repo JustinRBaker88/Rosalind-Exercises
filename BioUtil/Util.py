72ee5a741ad64e1f365a0bdf0d8004692eb92643
Justin Baker <JustinRBaker88@gmail.com>
2018-01-23 13:53:59 -0800

    Updated Util to include a function to produce K-mer strings.

--- BioUtil/Util.py
+++ BioUtil/Util.py
@@ -4,7 +4,6 @@ def parse_fasta(fasta_path):
     parsed = []
     data = open(fasta_path,'r')
     
-    temp_dna = ""
     index = -1
     for line in data:
         if (line[0] == '>'):
@@ -15,3 +14,19 @@ def parse_fasta(fasta_path):
     
     data.close()
     return parsed
+
+# Accepts two parameters and returns a corresponding DNA K-Mer string
+# Index represents the order of the K-Mer and k represents the number
+# bases in the K-Mer. 
+def get_dna_kmer(index, k):
+    kmer = []
+    nucleobase = ["A", "C", "T", "G"]
+    
+    for x in range(k):
+        kmer.append(nucleobase[0])
+    
+    for i in range(k-1, -1, -1):
+        kmer[i] = nucleobase[int(index % 4)]
+        index = int(index/4)
+        
+    return "".join(kmer)
